4a48063cbb5600cb93b00eed443e4f329b29daae
Pankaj Goyal <pkjg1807@gmail.com>
2019-06-17 00:47:45 +0530

    added all files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
+import Cart from './Components/Cart/Cart.js';
+import Menu from './Components/Menu/Menu.js';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let a=-1;
+class App extends React.Component{
+  constructor(){
+    super();
+    this.state=
+    {
+      veg:[],
+      nonveg:[],
+      cartitems:[]
+    }
+  }
+  componentDidMount(){
+    fetch('http://demo0764762.mockable.io/food')
+    .then(response=>response.json())
+    .then(data=>{
+      this.setState({veg:data.veg,nonveg:data.nonveg})
+    .catch(err=>console.log(err))
+    })
+  }
+  onAddition = (event)=>{
+    const id = event.target.value;
+    this.state.cartitems.push(parseInt(id));
+    this.setState({cartitems:this.state.cartitems})
+    //console.log(this.state.cartitems);
+  }
+  render(){
+    return (
+      <div className="App">
+        <header className="header">
+          React Task
+        </header>
+        <Cart veg={this.state.veg} nonveg={this.state.nonveg} selecteditems={this.state.cartitems} />
+        <Menu vegitems={this.state.veg} nonvegitems={this.state.nonveg} addToCart={this.onAddition} />
+      </div>
+    ); 
+  }
+  
 }
 
 export default App;
